b6eb1b46f84e50ccf55b4696110ff79960b27fa9
jitanshuraut <jayeshastha@gmail.com>
2022-11-19 10:16:19 +0530

    Homepage🔥👍

---
+++ src/pages/Home.jsx
@@ -0,0 +1,133 @@
+import React from 'react'
+
+
+function Home() {
+  return (
+  <>
+  <div className="firstSection" style={{backgroundImage:"url(image.png)"}}>
+          <div className="navBar">
+            <div className="logo">
+              <img src="Logo.png" className="logo2" alt="Logo" />
+            </div>
+            <div className="menus">
+              <ul>
+                <li>Stocks</li>
+                <li>Predictor</li>
+                <li>Profile</li>
+                <li>About us</li>
+              </ul>
+            </div>
+            <div className="Sign up"><button className="button-43 btn6" role="button">Sign In</button></div>
+          </div>
+          <div className="section">
+            <div className="firstLeft">
+              <div className="line">
+                <div className="head1">Trading</div>
+                <div className="head2">Platform</div>
+              </div>
+              <div className="head3">of the Future!</div>
+              <div className="text">Always an informed investment decision. <br />
+                First you prepare, then you go for it.</div>
+              <div className="btn" id="btn2"><button className="button-43" role="button">Sign In</button></div>
+            </div>
+            <div className="firstRight">
+              <img className="firstImage" src="mobiletrade.png" alt="image" />
+            </div>
+          </div>
+        </div>
+        <div className="second">
+          <div className="left2">
+            <img src="computer2.jpg" alt="img" className="img3" />
+          </div>
+          <div className="right2">
+            <div className="head1 head5">Becoming No #1 For <br /> Investment Needs</div>
+            <div className="text text2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna.</div>
+            <div className="head1 head5 head6">Our Vision</div>
+            <hr />
+            <div className="text text2">Accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.</div>
+            <div className="head1 head5 head6">Our Mission</div>
+            <hr />
+            <div className="text text2">Perspiciatis unde omnis iste natus error volupta accusantium.</div>
+          </div>
+        </div>
+        <div className="third second">
+          <div className="left2">
+            <img src="men.jpg" alt="img" className="img3" />
+          </div>
+          <div className="right2">
+            <div className="head1 head5">Find Stocks According <br /> To Your Criteria</div>
+            <div className="text text2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna.</div>
+            <div className="f1">
+              <img src="search.png" alt="Search" width="50px" className="icon" />
+              <div className="info2">
+                <div className="head1 head5 head6">Choose Your Stock</div>
+                <div className="text text2">Accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div className="f1">
+              <img src="chart.png" alt="Search" width="50px" className="icon" />
+              <div className="info2">
+                <div className="head1 head5 head6">Detailed Comparison</div>
+                <div className="text text2">Accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div className="f1">
+              <img src="wallet.png" alt="Search" width="50px" className="icon" />
+              <div className="info2">
+                <div className="head1 head5 head6">Buy Your Shares</div>
+                <div className="text text2">Accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div className="f1">
+              <img src="selling.png" alt="Search" width="50px" className="icon" />
+              <div className="info2">
+                <div className="head1 head5 head6">Sell Anytime</div>
+                <div className="text text2">Accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.
+                </div>
+              </div>
+            </div>
+            <hr />
+          </div>
+        </div>
+        <div className="fourth second">
+          <div className="left2">
+            <img src="image7.png" alt="img" className="img3" />
+          </div>
+          <div className="right2">
+            <div className="head1 head5">What is us?</div>
+            <div className="text text2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium expedita, eaque atque libero enim reiciendis repellendus at quas laudantium voluptatem deleniti sit molestias adipisci tenetur consequuntur minima nulla ullam iste!</div>
+            <div className="Sign up"><button className="button-43 btn6 btn8" role="button">Sign In</button></div>
+          </div>
+        </div>
+        <footer>
+          <div className="foot" style={{backgroundImage:"url(background5.png)"}}>
+            <img src="Logo.png" alt="" />
+            <div className="menus" id="nav2">
+              <ul id="nav3">
+                <li>Stocks</li>
+                <li>Predictor</li>
+                <li>Profile</li>
+                <li>About us</li>
+              </ul>
+            </div>
+            <div className="social">
+              <i className="fa fa-linkedin-square" style={{fontSize: '36px'}} />
+              <i className="fa fa-github" style={{fontSize: '36px'}} />
+            </div>
+          </div>
+        </footer>
+      
+  </>
+  )
+}
+
+export default Home
